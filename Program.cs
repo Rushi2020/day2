bd93de2ea05b4ef423fe0a3817bbab83f4e49da4
Rushi <189489rishi@gmail.com>
2022-02-10 10:34:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day2
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
